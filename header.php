736a30ed24caf78aad49665aaa123feec3a58080
adavydov1 <63337035+adavydov1@users.noreply.github.com>
2021-06-03 19:04:14 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,124 @@
+<header class="fusion-header-wrapper fusion-is-sticky" style="">
+    <div class="fusion-header-v1 fusion-logo-left fusion-sticky-menu- fusion-sticky-logo- fusion-mobile-logo-1  fusion-mobile-menu-design-modern">
+        <div class="fusion-header-sticky-height" style="height: 130px; display: block; overflow: visible;">
+        </div>
+        <div class="fusion-header fusion-sticky-shadow" style="height: 130px; top: 0px; overflow: visible;">
+            <div class="fusion-row">
+                <div class="fusion-logo" data-margin-top="10px" data-margin-bottom="0px" data-margin-left="0px" data-margin-right="0px" style="">
+
+
+                    <!--<a class="fusion-logo-link"  href="/" >-->
+
+
+
+                    <p class="fusion-logo-link" <!--="" standard="" logo="" --="">
+                        <img src="/img/logo-coffee.png" style="width:250px!important;" srcset="/img/logo-coffee.png 1x, /img/logo-coffee.png 2x" width="500" height="170" style="max-height:102px;height:auto;" alt="СЦ Ремонт Кофемашин Logo" retina_logo_url="/img/logo-coffee.png" class="fusion-standard-logo">
+                        <!-- mobile logo -->
+                        <noscript><img src="img/logo-coffee-mob.png" srcset="/img/logo-coffee-mob.png 1x, /img/logo-coffee-mob.png 2x" width="150" height="102" style="max-height:102px;height:auto;" alt="СЦ Ремонт Кофемашин Logo" retina_logo_url="/img/logo-coffee-mob.png" class="fusion-mobile-logo" /></noscript>
+                        <img src="img/logo-coffee-mob.png" style="width:150px!important;" data-src="/img/logo-coffee-mob.png" data-srcset="/img/logo-coffee-mob.png 1x, /img/logo-coffee-mob.png 2x" width="150" height="102" style="max-height:102px;height:auto;" alt="СЦ Ремонт Кофемашин Logo" retina_logo_url="/img/logo-coffee-mob.png" class="fusion-mobile-logo lazyloaded" srcset="/img/logo-coffee-mob.png 1x, /img/logo-coffee-mob.png 2x">
+                    </p>
+                </div>
+
+                <nav class="fusion-main-menu" aria-label="Main Menu" style="">
+                    <ul class="fusion-checklist fusion-checklist-1 mob-tel-1" style="font-size:16px;line-height:23.8px; margin-right:5rem">
+                        <li class="fusion-li-item">
+                            <span style="height:23.8px;width:23.8px;display:flex;align-items:center;margin-top:1px;" class="icon-wrapper circle-no">
+                                <i class="fusion-li-icon fa fa-phone" style="color:#717171;font-size:17px;"></i>
+                            </span>
+                            <div class="fusion-li-item-content" style="margin-left:24.6px;"><a href="tel:+74991130524">+7 (499) 113-05-24</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul role="menubar" id="menu-construction-main-menu" class="fusion-menu">
+                        <li role="menuitem" id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-1188 current_page_item menu-item-22">
+                            <a href="index.html" class="fusion-arrow-highlight"><span class="menu-text">Главная<span class="fusion-arrow-svg"><svg height="12px" width="23px">
+                                            <path d="M0 0 L11.5 12 L23 0 Z" fill="#ffffff"></path>
+                                        </svg></span></span></a>
+                        </li>
+                        <li role="menuitem" id="menu-item-1848" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1848"><a href="pages/price.php.html" class="fusion-arrow-highlight"><span class="menu-text">Цены<span class="fusion-arrow-svg"><svg height="12px" width="23px">
+                                            <path d="M0 0 L11.5 12 L23 0 Z" fill="#ffffff"></path>
+                                        </svg></span></span></a></li>
+                        <li role="menuitem" id="menu-item-3625" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-3625 fusion-dropdown-menu" style=""><a class="fusion-arrow-highlight" aria-haspopup="true"><span class="menu-text">Неисправности<span class="fusion-arrow-svg"><svg height="12px" width="23px">
+                                            <path d="M0 0 L11.5 12 L23 0 Z" fill="#ffffff"></path>
+                                        </svg></span><span class="fusion-dropdown-svg"><svg height="12px" width="23px">
+                                            <path d="M0 12 L11.5 0 L23 12 Z" fill="#ffffff"></path>
+                                        </svg></span></span></a>
+                            <ul role="menu" class="sub-menu">
+                                <li role="menuitem" id="menu-item-3630" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3630 fusion-dropdown-submenu">
+                                    <a href="pages/malfunction/techet-voda-iz-kofemashiny.php.html" class="fusion-arrow-highlight"><span>Течет вода</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3629" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3629 fusion-dropdown-submenu">
+                                    <a href="pages/malfunction/kofemashina-ne-vklyuchaetsya.php.html" class="fusion-arrow-highlight"><span>Не включается</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3628" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3628 fusion-dropdown-submenu">
+                                    <a href="pages/malfunction/kofemashina-ne-nalivaet-kofe.php.html" class="fusion-arrow-highlight"><span>Не идет кофе или вода</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3627" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3627 fusion-dropdown-submenu">
+                                    <a href="pages/malfunction/kofemashina-ne-greet-vodu.php.html" class="fusion-arrow-highlight"><span>Не греет воду</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3626" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3626 fusion-dropdown-submenu">
+                                    <a href="pages/malfunction/kofemashina-ne-melet.php.html" class="fusion-arrow-highlight"><span>Не
+                                            мелет кофе</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li role="menuitem" id="menu-item-3641" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-3641 fusion-dropdown-menu" style=""><a class="fusion-arrow-highlight" aria-haspopup="true"><span class="menu-text">Сервис<span class="fusion-arrow-svg"><svg height="12px" width="23px">
+                                            <path d="M0 0 L11.5 12 L23 0 Z" fill="#ffffff"></path>
+                                        </svg></span><span class="fusion-dropdown-svg"><svg height="12px" width="23px">
+                                            <path d="M0 12 L11.5 0 L23 12 Z" fill="#ffffff"></path>
+                                        </svg></span></span></a>
+                            <ul role="menu" class="sub-menu">
+                                <li role="menuitem" id="menu-item-3652" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3652 fusion-dropdown-submenu">
+                                    <a href="pages/service/diagnostika-kofemashiny.php.html" class="fusion-arrow-highlight"><span>Диагностика</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-1791" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1791 fusion-dropdown-submenu">
+                                    <a href="pages/service/dekaltsinatsiya-kofemashiny.php.html" class="fusion-arrow-highlight"><span>Декальцинация</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3655" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3655 fusion-dropdown-submenu">
+                                    <a href="pages/service/remont-pompy-dlya-kofemashiny.php.html" class="fusion-arrow-highlight"><span>Ремонт помпы</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3666" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3666 fusion-dropdown-submenu">
+                                    <a href="pages/service/zamena-datchika-vody.php.html" class="fusion-arrow-highlight"><span>Замена датчика воды</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3665" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3665 fusion-dropdown-submenu">
+                                    <a href="pages/service/zamena-prokladki-kofemashiny.php.html" class="fusion-arrow-highlight"><span>Замена прокладок и
+                                            уплотнителей</span></a>
+                                </li>
+                                <li role="menuitem" id="menu-item-3669" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3669 fusion-dropdown-submenu">
+                                    <a href="pages/service/chistka-gidrosistemy-kofemashiny.php.html" class="fusion-arrow-highlight"><span>Чистка гидросистемы</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li role="menuitem" id="menu-item-17" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-17 fusion-menu-item-button fusion-last-menu-item">
+                            <a href="pages/kontakty.php.html" class="fusion-arrow-highlight"><span class="menu-text fusion-button button-default button-large">Контакты</span></a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="fusion-mobile-menu-icons">
+
+                    <a href="index.html#" class="fusion-icon fusion-icon-bars" aria-label="Toggle mobile menu"></a>
+
+
+                    <ul class="fusion-checklist fusion-checklist-1 mob-tel" style="font-size:16px;line-height:23.8px; margin-right:5rem">
+                        <li class="fusion-li-item" style="display:flex;justify-content:flex-end;margin-top:1px;">
+                            <span style="height:23.8px;width:23.8px;display:flex;align-items:center;margin-top:1.5px;" class="icon-wrapper circle-no">
+                                <i class="fusion-li-icon fa fa-phone" style="color:#717171;font-size:22px;"></i>
+                            </span>
+                            <div class="fusion-li-item-content" style="margin-left:-13.6px;"><a href="tel:+74991130524" style="color:#000!important;">+7 (499) 113-05-24</a>
+                            </div>
+                        </li>
+                    </ul>
+
+
+                </div>
+
+
+                <nav class="fusion-mobile-nav-holder fusion-mobile-menu-text-align-left" style="display: none;">
+
+                </nav>
+
+            </div>
+        </div>
+    </div>
+    <div class="fusion-clearfix"></div>
+</header>
